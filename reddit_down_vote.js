35973c1207be743075cf029ffdda87d3c265fa01
phanku <phanku@users.noreply.github.com>
2016-10-05 16:49:13 -0400

    v2

--- reddit_down_vote.js
+++ reddit_down_vote.js
@@ -1,6 +1,6 @@
-var items = $('.arrow.down');
-function doit(target) {if ($(target).parent('.midcol').hasClass('unvoted')) {$(target).click();}next();}
-function next() {if (items.length > 0) {var next = items.splice(0, 1);setTimeout(function() {doit(next[0])},gr());} else {console.log('done');}}
-function ggr() {return Math.floor(Math.random() * (3000 - 700 + 1)) + 700;}
-function gr() {return Math.floor(Math.random() * (ggr() - 500 + 1)) + 400;}
-next();
+var i = $('.arrow.down');
+function d(t) {if ($(t).parent('.midcol').hasClass('unvoted')) {$(t).click();}n();}
+function n() {if (i.length > 0) {var n = items.splice(0, 1);setTimeout(function() {d(n[0])},r());} else {console.log('done');}}
+function s() {return Math.floor(Math.random() * (3000 - 700 + 1)) + 700;}
+function r() {return Math.floor(Math.random() * (s() - 500 + 1)) + 400;}
+n();
