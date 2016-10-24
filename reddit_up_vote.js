0d6c634990e01dbad659fb72b31882fe0b6d4395
Joseph Pahl <nophreak@gmail.com>
2016-10-24 10:49:03 -0400

    stuffs

--- reddit_up_vote.js
+++ reddit_up_vote.js
@@ -1,4 +1,4 @@
-var items = $('.arrow.down');
+var items = $('.arrow.up');
 function doit(target) {if ($(target).parent('.midcol').hasClass('unvoted')) {$(target).click();}next();}
 function next() {if (items.length > 0) {var next = items.splice(0, 1);setTimeout(function() {doit(next[0])},gr());} else {console.log('done');}}
 function ggr() {return Math.floor(Math.random() * (3000 - 700 + 1)) + 700;}
