d3a4b41ca5c8cb3407c87c3198bf82a025f30d13
Joseph Pahl <phanku@users.noreply.github.com>
2017-11-30 14:57:43 -0500

    Update reddit_up_vote.js

--- reddit_up_vote.js
+++ reddit_up_vote.js
@@ -1,6 +1,6 @@
-var items = $('.arrow.up');
-function doit(target) {if ($(target).parent('.midcol').hasClass('unvoted')) {$(target).click();}next();}
+var items = $('.arrow.down');
+function doit(target) {if ($(target).parent('.midcol').hasClass('unvoted')) {window.scrollTo(0, $(target).position().top - 200); $(target).click();}next();}
 function next() {if (items.length > 0) {var next = items.splice(0, 1);setTimeout(function() {doit(next[0])},gr());} else {console.log('done');}}
-function ggr() {return Math.floor(Math.random() * (3000 - 700 + 1)) + 700;}
+function ggr() {return Math.floor(Math.random() * (5000 - 700 + 1)) + 700;}
 function gr() {return Math.floor(Math.random() * (ggr() - 500 + 1)) + 400;}
 next();
